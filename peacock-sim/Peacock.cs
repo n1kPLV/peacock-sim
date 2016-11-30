2b43cd7d23a9bc4ddea30cb2adbfd2624e6bb9de
n1kPLV <daniel@maeckelmann.de>
2016-11-30 19:52:45 +0100

    half-way finished

--- peacock-sim/Peacock.cs
+++ peacock-sim/Peacock.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace peacocksim
 {
     class Peacock
     {
+        //Defining fields
         private int feathers;
         private int attractivity;
         private int resistance;
         private bool dead;
         private bool hurt;
         private Random rand;
+        private int[] successors;
 
         //Constructor for a Peacock
-
         public Peacock(int feathers, Random randomiser)
         {
             this.feathers = feathers;
@@ -25,34 +24,50 @@ namespace peacocksim
             dead = false;
             hurt = false;
             rand = randomiser;
+            successors = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         }
-        public void movePartner()
+
+        //doing auto move(Partner)
+        public void movePartner(int round)
         {
             //variables
             int diceroll1 = rand.Next(1, 6);
             int diceroll2 = rand.Next(1, 6);
             int tmpattractivity = attractivity;
+            bool endofround = false;
 
-            //TODO: game logic
-            if (diceroll1 != 6)
+            //don't do anything if dead or hurt
+            if(dead | hurt)
+            {
+                endofround = true;
+            }
+            
+            //check if rolled a 6 and else try to sum up
+            if (diceroll1 != 6 & !endofround)
             {
                 if (tmpattractivity - (6 - diceroll1) >= 0)
                 {
                     tmpattractivity = tmpattractivity - (6 - diceroll1);
                 }
+                //
                 else
                 {
-                    
+                    endofround = true;
                 }
             }
-            if (true)
+            if (!endofround & diceroll2 >= 4)
             {
-
+                successors[round-1] = tmpattractivity + 1;
             }
-                       
+            else if (!endofround & tmpattractivity - (4 - diceroll2) >= 0)
+            {
+                tmpattractivity = tmpattractivity - (4 - diceroll2);
+                successors[round-1] = tmpattractivity + 1;
+            }
+            
         }
 
-        public void moveEnemy()
+        public void moveEnemy(int round)
         {
             //variables
             int diceroll1 = rand.Next(1, 6);
@@ -60,6 +75,40 @@ namespace peacocksim
 
             //TODO: game logic
 
+        }
+        
+        public bool getHurt()
+        {
+            return hurt;
+        }
+
+        public bool getDeath()
+        {
+            return dead;
+        }
+
+        public string getDebug()
+        {
+            StringBuilder tmp = new StringBuilder("[DEBUG] ");
+            tmp.Append(" feathers: ");
+            tmp.Append(feathers);
+            tmp.Append(" attractivity: ");
+            tmp.Append(attractivity);
+            tmp.Append(" resistance: ");
+            tmp.Append(resistance);
+            tmp.Append("");
+            tmp.Append("");
+            return tmp.ToString();
+        }
+
+        public int getTotalSuccessors()
+        {
+            return successors.Sum();
+        }
+
+        public int getFeathers()
+        {
+            return feathers;
         }
     }
 }
--- peacock-sim/Program.cs
+++ peacock-sim/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace peacocksim
 {
     class Program
     {
+        private static Random rand = new Random();
         static void Main(string[] args)
         {
+
+            //Test Code
+            Console.Write("feathers:? ");
+            Peacock pfau = new Peacock(Int32.Parse(Console.ReadLine()), rand);
+            while (true)
+            {
+                Console.WriteLine(pfau.getDebug());
+                Console.Write("round partner:? ");
+                pfau.movePartner(Int32.Parse(Console.ReadLine()));
+                Console.WriteLine(pfau.getTotalSuccessors());
+                Console.WriteLine(pfau.getDebug());
+                
+            }
         }
     }
 }
