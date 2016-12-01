b19e050559bff55b1ddae361015ee09ba1ca3946
n1kPLV <daniel@maeckelmann.de>
2016-12-01 18:30:31 +0100

    added Readme & License; added more arg

--- peacock-sim/Program.cs
+++ peacock-sim/Program.cs
@@ -7,12 +7,62 @@ namespace peacocksim
     class Program
     {
         private static Random rand = new Random();
+        ///first arg: needed, number of samples
+        ///second arg: optional, share of partners
+        ///third arg: optional, share of enemies
+        ///4th arg: optional, outputfile
         static void Main(string[] args)
         {
-            string file = Path.GetTempFileName() + ".csv";
-            StreamWriter log = new StreamWriter(file);
+            string file;
+            StreamWriter log;
             int arg1 = 0;
+            int arg2 = 1;
+            int arg3 = 1;
 
+            if (args.Length >= 4)
+            {
+                if (File.Exists(args[3]))
+                {
+                    Console.WriteLine("Given File exists. Ignoring");
+                    file = Path.GetTempFileName() + ".csv";
+                }
+                else
+                {
+                    try
+                    {
+                        File.Create(args[4]);
+                        file = args[4];
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("File could not be created. Ignoring");
+                        file = Path.GetTempFileName() + ".csv";
+                    }
+                }
+                
+            }
+            else
+            {
+                file = Path.GetTempFileName() + ".csv";
+            }
+            if (args.Length >= 2)
+            {
+                if (!int.TryParse(args[1], out arg2))
+                {
+                    Console.WriteLine("second argument set invalid");
+                }
+                
+            }
+            if (args.Length >= 3)
+            {
+                if (!int.TryParse(args[2], out arg3))
+                {
+                    Console.WriteLine("third argument set invalid");
+                }
+            }
+            
+            
+            log = new StreamWriter(file);
 
             if (!(args.Length == 0 || !int.TryParse(args[0], out arg1)))
             {
@@ -25,7 +75,8 @@ namespace peacocksim
                     {
                         foreach (Peacock pfau in pfauen)
                         {
-                            if (rand.Next(0, 2) == 0)
+                            int rr = rand.Next(1, arg2 + arg3);
+                            if (rr >= arg3)
                             {
                                 pfau.moveEnemy(i);
                             }
